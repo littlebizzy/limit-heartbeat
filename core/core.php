575657776acad19d8800b28c4e72411d3b380b41
Pau <pauiglesias@gmail.com>
2018-11-27 14:28:44 +0100

    Initial files from Plugin Boilerplate plugin.

---
+++ core/core.php
@@ -0,0 +1,33 @@
+<?php
+
+// Subpackage namespace
+namespace LittleBizzy\LimitHeartbeat\Core;
+
+// Aliased namespaces
+use \LittleBizzy\LimitHeartbeat\Helpers;
+
+/**
+ * Core class
+ *
+ * @package Limit Heartbeat
+ * @subpackage Core
+ */
+final class Core extends Helpers\Singleton {
+
+
+
+	/**
+	 * Pseudo constructor
+	 */
+	protected function onConstruct() {
+
+		// Factory object
+		//$this->plugin->factory = new Factory($this->plugin);
+
+		// Attempt to run an object
+		//$this->plugin->factory->myObject()
+	}
+
+
+
+}
