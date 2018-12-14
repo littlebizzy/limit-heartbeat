d9059d76c3d84cff7139c73a52c0d6a5db56a139
Pau <pauiglesias@gmail.com>
2018-12-14 14:41:15 +0100

    Updated plugin meta and readme from 1.0.0 version.

--- limit-heartbeat.php
+++ limit-heartbeat.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Limit Heartbeat
-Plugin URI: https://www.littlebizzy.com/plugins/[plugin-slug]
+Plugin URI: https://www.littlebizzy.com/plugins/limit-heartbeat
 Description: Limits the Heartbeat API in WordPress to certain areas of the site (and a longer pulse interval) to reduce AJAX queries and improve resource usage.
 Version: 1.1.0
 Author: LittleBizzy
@@ -9,7 +9,7 @@ Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 WC requires at least: 3.3
-WC tested up to: 3.4
+WC tested up to: 3.5
 PBP Version: 1.1.0
 */
 
